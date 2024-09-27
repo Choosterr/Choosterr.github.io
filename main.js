56c20fdef7cf776dd989ce6e32ff9b9787da7fe3
Chooster <chooster6@gmail.com>
2024-09-26 23:37:27 -0700

    Changed to main.js and added blank vis.js

---
+++ main.js
@@ -0,0 +1,20 @@
+const svg = document.getElementById('vis');
+
+function randomize() {   
+    //create a random number of circles of random sizes filled with random colors
+    for (let i=0; i<Math.floor(Math.random() * 15) + 5; i++) {
+        const circleGen = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+        circleGen.setAttribute('cx', Math.floor(Math.random() * 700) + 50);
+        circleGen.setAttribute('cy', Math.floor(Math.random() * 500) + 50);
+        circleGen.setAttribute('r', Math.floor(Math.random() * 80) + 10);
+        
+        const r = Math.floor(Math.random() * 200) + 50;
+        const g = Math.floor(Math.random() * 200) + 50;
+        const b = Math.floor(Math.random() * 200) + 50;
+        const rgb = 'rgb('+ r + ', ' + g + ', '+ b +')';
+
+        circleGen.setAttribute('fill', rgb);
+        svg.appendChild(circleGen);
+    }
+}
+    
